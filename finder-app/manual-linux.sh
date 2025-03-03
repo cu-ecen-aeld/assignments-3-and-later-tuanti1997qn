aeba0da58c55a3ee83088c94aa7351a8db3e2f1a
Tuan Pham <tuanti1997qn@gmail.com>
2025-03-03 08:10:14 -0800

    Fix build script

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -96,7 +96,7 @@ sudo mknod -m 666 dev/null c 1 3
 
 # TODO: Clean and build the writer utility
 cd ${WORKSPACE}
-make CROSS_COMPILE=${CROSS_COMPILE} clean
+#make CROSS_COMPILE=${CROSS_COMPILE} clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
